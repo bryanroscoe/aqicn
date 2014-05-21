d1e9bdd8a99473c5b4e67dc1ab1d530e4957907c
Bryan Roscoe <BryanRoscoe@gmail.com>
2014-05-20 21:23:34 -0500

    Now writing to files with dates

--- aqicn.py
+++ aqicn.py
@@ -6,8 +6,16 @@ import re
 from builtins import len
 import requests
 from bs4 import BeautifulSoup
+import time
+import datetime
+import os
 
+def ensure_dir(f):
+    d = os.path.dirname(f)
+    if not os.path.exists(d):
+        os.makedirs(d)
 
+datetime.time
 print("Getting the main page")
 fullMap = urllib.request.urlopen("http://aqicn.org/map/world/")
 
@@ -25,6 +33,23 @@ print("There are" ,len(cities) , "cities")
 print("Populating the city URLS")
 cityPopupUrls = []
 for i, city in enumerate(cities):
+    
+    cityTime = time.strptime(city['utime'], "%A %H:%M")
+    curDateTime = datetime.datetime.utcnow()
+    cityDateTime = curDateTime.replace(hour=cityTime.tm_hour, minute=cityTime.tm_min)
+    diffDays = curDateTime.weekday() -cityTime.tm_wday
+    
+    if diffDays == -1 or diffDays == 6:
+        cityDateTime += datetime.timedelta(days=1)
+    elif diffDays > 0:
+        cityDateTime += datetime.timedelta(days=-diffDays)
+    elif diffDays < 0:
+        cityDateTime += datetime.timedelta(days=diffDays)
+    city['dateTime'] = cityDateTime
+    
+    if (i+1)%750 !=0:
+        continue
+    print(city)
     print("Scraping "+ city["city"] , i+1, "of",len(cities), city["g"])
     city["popupURL"]=("http://aqicn.info/json/mapinfo/@" + str(city["x"]))
     cityDetailPage= urllib.request.urlopen(city["popupURL"]).read().decode("utf-8")
@@ -32,23 +57,55 @@ for i, city in enumerate(cities):
     
     if city["detailURL"]:
         city["detailURL"] = city["detailURL"].group(0).replace("info", "org", 1)
-        #print("Found", city["detailURL"])
+
         
         headers = {
                 'User-agent': 'Mozilla/5.0'
         }
         
         page = requests.get(city["detailURL"],allow_redirects=False, headers=headers)
-        page = requests.get(page.headers.get('location') + "m",allow_redirects=True, headers=headers)
-        #print("Pages",page.history, page.url, page.status_code, page.headers.get('location'))
+
+        if page.headers.get('location'):
+            page = requests.get(page.headers.get('location') + "m",allow_redirects=True, headers=headers)
+        else:
+            soup = BeautifulSoup(page.text)
+            titleLink = soup.find(id="aqiwgttitle1")
+            if titleLink:
+                print("titleLink", titleLink['href'])
+                page = requests.get(titleLink['href'] + "m",allow_redirects=True, headers=headers)
         
         soup = BeautifulSoup(page.text)
         city["data"] = {}
-        city["data"]["pm25"] = soup.find("td", {"id" : "cur_pm25"}).find("div").contents[0]
-        print("pm25" ,city["data"]["pm25"])
-        city["data"]["pm10"] = soup.find("td", {"id" : "cur_pm10"}).find("div").contents[0]
-        print("pm10" ,city["data"]["pm10"])
+        curList = soup.find_all("td", {"id" : re.compile('^cur_')})
+        
+        if not curList:
+            continue
+        
+        for cur in curList:
+            curId = cur['id']
+            curIdStripped = curId[4:]
+            curDiv = cur.find('div')
+            if not curDiv:
+                continue
+            city['data'][curId] = curDiv.contents[0]
+            print(curId, city['data'][curId])
+            curFilename = 'Data/AQICN/' + str(curIdStripped) + "/"
+            curFilename += cityDateTime.strftime("%Y/%m/%d/")
+            curFilename += cityDateTime.strftime("%Y%m%d")
+            curFilename += str(curIdStripped) + ".csv"
+            print(curFilename)
+            ensure_dir(curFilename)
+            f = open(curFilename, 'a')
+            f.write(cityDateTime.strftime("%Y%m%d%H%M") + ",")
+            f.write(str(city['g'][0]) + ",")
+            f.write(str(city['g'][1]) + ",")
+            f.write(str(city['data'][curId]) + "\n")
+            f.close()
+            
+            
 
     else:
         print('Not found')
+        
+
 
