802d058bd33366c350a14f8963277b2893a146c6
Bryan Roscoe <BryanRoscoe@gmail.com>
2014-05-21 19:58:12 -0500

    Cleaned up code and improved time calculations

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
 aqicn
 =====
 #####Scraper for aqicn.org
-###Written in [Python 3.4](https://www.python.org/downloads/release/python-34/)
 
+####How To use
+
+Data will stored like this
+[directory]/Data/AQICN/Variable/YYYY/MM/yyyymmdd-variable.csv
+You can change the base [directory] by editing the script. The variable is at the top.
+
+
+
+###Written in [Python 3.4](https://www.python.org/downloads/release/python-34/)
 
 ####Libraries
 Uses [PyPi](https://pypi.python.org/pypi) Versions of
--- aqicn.py
+++ aqicn.py
@@ -9,98 +9,111 @@ from bs4 import BeautifulSoup
 import time
 import datetime
 import os
+from math import ceil
 
-def ensure_dir(f):
+directory= "./"
+
+#Function to make sure a directory exists
+def ensureDir(f):
     d = os.path.dirname(f)
     if not os.path.exists(d):
         os.makedirs(d)
 
-datetime.time
-print("Getting the main page")
-fullMap = urllib.request.urlopen("http://aqicn.org/map/world/")
-
-fullMap = fullMap.read().decode("utf-8")
-
-print("Finding the url")
-fullMapJsonString = re.search("(?<=mapInitWithData\()\[.*\](?=\))", fullMap)
-
-#print(fullMapJsonString.group(0))
-cities = None
-if fullMapJsonString:
-    cities = json.loads(fullMapJsonString.group(0))
+def getTime(utime, long):
+    cityTime = time.strptime(utime, "%A %H:%M")
     
-print("There are" ,len(cities) , "cities")
-print("Populating the city URLS")
-cityPopupUrls = []
-for i, city in enumerate(cities):
-    
-    cityTime = time.strptime(city['utime'], "%A %H:%M")
     curDateTime = datetime.datetime.utcnow()
+
+    curDateTime += datetime.timedelta(hours=ceil(float(long)/15))
     cityDateTime = curDateTime.replace(hour=cityTime.tm_hour, minute=cityTime.tm_min)
     diffDays = curDateTime.weekday() -cityTime.tm_wday
     
-    if diffDays == -1 or diffDays == 6:
-        cityDateTime += datetime.timedelta(days=1)
-    elif diffDays > 0:
+    
+    if diffDays > 0:
         cityDateTime += datetime.timedelta(days=-diffDays)
     elif diffDays < 0:
         cityDateTime += datetime.timedelta(days=diffDays)
-    city['dateTime'] = cityDateTime
+    return cityDateTime
     
-    if (i+1)%750 !=0:
+#First we must get the main map page
+print("Getting the main page")
+fullMap = urllib.request.urlopen("http://aqicn.org/map/world/")
+fullMap = fullMap.read().decode("utf-8")
+
+#Find the json embedded in the main page
+print("Finding the json")
+fullMapJsonString = re.search("(?<=mapInitWithData\()\[.*\](?=\))", fullMap)
+
+#Parse the json
+cities = None
+if fullMapJsonString:
+    cities = json.loads(fullMapJsonString.group(0))
+
+#Loop through the cities and load each one
+print("There are" ,len(cities) , "cities")
+cityPopupUrls = []
+for i, city in enumerate(cities):
+    
+    city['dateTime'] = getTime(city['utime'], str(city['g'][1]))
+
+    ##Remove this line to scrape all cities
+    if (i+1)%500 !=0:
         continue
-    print(city)
+    
     print("Scraping "+ city["city"] , i+1, "of",len(cities), city["g"])
+    #Get the details url from the 
     city["popupURL"]=("http://aqicn.info/json/mapinfo/@" + str(city["x"]))
     cityDetailPage= urllib.request.urlopen(city["popupURL"]).read().decode("utf-8")
     city["detailURL"] = re.search("http://aqicn\.info/[^\']*", cityDetailPage)
     
     if city["detailURL"]:
+        #Load the detail page and get the redirect page
         city["detailURL"] = city["detailURL"].group(0).replace("info", "org", 1)
-
         
         headers = {
                 'User-agent': 'Mozilla/5.0'
         }
         
         page = requests.get(city["detailURL"],allow_redirects=False, headers=headers)
-
+        
+        #Attempt to get the redirect page. If that fails we will assume the page the did not redirect and scrapte the url
         if page.headers.get('location'):
             page = requests.get(page.headers.get('location') + "m",allow_redirects=True, headers=headers)
         else:
             soup = BeautifulSoup(page.text)
             titleLink = soup.find(id="aqiwgttitle1")
             if titleLink:
-                print("titleLink", titleLink['href'])
                 page = requests.get(titleLink['href'] + "m",allow_redirects=True, headers=headers)
         
         soup = BeautifulSoup(page.text)
         city["data"] = {}
         curList = soup.find_all("td", {"id" : re.compile('^cur_')})
-        
+        #Go on to the next city if we don't find anything
         if not curList:
+            print("Nothing found for", city['city'])
             continue
-        
+        #Loop through all the variables for this city
+        savedVars = ""
         for cur in curList:
             curId = cur['id']
             curIdStripped = curId[4:]
+            savedVars += curIdStripped + ","
             curDiv = cur.find('div')
             if not curDiv:
                 continue
             city['data'][curId] = curDiv.contents[0]
-            print(curId, city['data'][curId])
-            curFilename = 'Data/AQICN/' + str(curIdStripped) + "/"
-            curFilename += cityDateTime.strftime("%Y/%m/%d/")
-            curFilename += cityDateTime.strftime("%Y%m%d")
+            curFilename = directory + '/' +'Data/AQICN/' + str(curIdStripped) + "/"
+            curFilename += city['dateTime'].strftime("%Y/%m/%d/")
+            curFilename += city['dateTime'].strftime("%Y%m%d")
             curFilename += str(curIdStripped) + ".csv"
-            print(curFilename)
-            ensure_dir(curFilename)
+            ensureDir(curFilename)
             f = open(curFilename, 'a')
-            f.write(cityDateTime.strftime("%Y%m%d%H%M") + ",")
+            f.write(city['dateTime'].strftime("%Y%m%d%H%M") + ",")
             f.write(str(city['g'][0]) + ",")
             f.write(str(city['g'][1]) + ",")
             f.write(str(city['data'][curId]) + "\n")
             f.close()
+        print("Saved", savedVars, "for city", city['city'])
             
             
 
